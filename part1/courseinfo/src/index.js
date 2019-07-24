f7073ae8c8b4b8eb06bc7b3130bc3a4134a102ca
GEOCOM\psilva <psilva@geocom.com.uy>
2019-07-24 08:11:32 -0300

    Step1, step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (props) =>
+{
+    return(
+    <>
+    <h1>{props.course}</h1>
+    </>)
+}
+const Content = (props) =>
+{
+    return(
+    <>
+    <Part part={props.part1} exercise={props.exercises1}></Part>
+    <Part part={props.part2} exercise={props.exercises2}></Part>
+    <Part part={props.part3} exercise={props.exercises3}></Part>
+    </>)
+}
+const Total = (props) =>
+{
+    return(
+    <>
+   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>)
+}
+const Part = (props) =>
+{
+    return(
+        <>
+          <p>
+            {props.part} {props.exercise}
+          </p>
+        </>)
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Content>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Total>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
